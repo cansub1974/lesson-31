fe9f6c21d1a70c9fa9232fcb408a102c14fad27a
cansub1974 <jasonalexandermckay@hotmail.com>
2020-05-18 14:49:57 -0400

    Updated checkout

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_1iAIUnVUkHz4cI52SKg5I0hu00zSlJl66t';
 
   const onToken = token => {
     axios({
